ae058fe7ec7edb80f0bf0293597c17b901ea1dec
Loïc <loic.feuga@gmail.com>
2014-12-04 20:49:35 +0100

    Mise en place des 1er header de Accueil, Famille,Camps

--- mafamille.php
+++ mafamille.php
@@ -1,12 +1,11 @@
 <?php include "php/header.php"; ?>
 
-
-     <div class="row" id="header_benevole" class=""> 
+  
+    <div class="row" id="header_famille" class=""> 
       <div class="col-lg-12">
-        C'est l'accueil
+        C'est ma famille  
       </div>
     </div>
-
 
 
 <?php include "php/footer.php"; ?>
--- php/header.php
+++ php/header.php
@@ -47,58 +47,13 @@
   <div class="col-lg-12">
     <div class="row">
       <div class="col-lg-12"> 
-        <div class="col-lg-2">Accueil</div>
-        <div class="col-lg-2">Ma famille</div>
-        <div class="col-lg-2">Les camps</div>
-        <div class="col-lg-2">Devenir bénévoles</div>
-        <div class="col-lg-2">Missions</div>
-        <div class="col-lg-2">Contact</div>
+        <div class="col-lg-2"><a href="./index.php" id="go_to_accueil">Accueil</a></div>
+        <div class="col-lg-2"><a href="./mafamille.php" id="go_to_famille">Ma famille</a></div>
+        <div class="col-lg-2"><a href="./lescamps.php" id="go_to_camp">Les camps</a></div>
+        <div class="col-lg-2"><a href="" id="go_to_benevole">Devenir bénévoles</a></div>
+        <div class="col-lg-2"><a href="" id="go_to_mission">Missions</a></div>
+        <div class="col-lg-2"><a href="" id="go_to_contact">Contact</a></div>
       </div>
     </div>
 
-    <div class="row" id="header_accueil" class=""> 
-      <div class="col-lg-12 header_multi">
-        C'est l'accueil  
-      </div>
-    </div>
-  
-    <div class="row" id="header_famille" class=""> 
-      <div class="col-lg-12 header_multi">
-        C'est ma famille  
-      </div>
-    </div>
-    <div class="row" id="header_camp" class=""> 
-      <div class="col-lg-12 header_multi">
-        C'est les camps  
-      </div>
-    </div>
-
-     <div class="row" id="header_benevole" class=""> 
-      <div class="col-lg-12 header_multi">
-        C'est pour devenir bénévoles
-      </div>
-    </div>
-
-     <div class="row" id="header_mission" class=""> 
-      <div class="col-lg-12 header_multi">
-        C'est pour les missions
-      </div>
-    </div>
-
-    
-  
- <div class="row" id="header_contact" class=""> 
-      <div class="col-lg-12 header_multi">
-        C'est les contacts
-      </div>
-    </div>
-
-
-
-  </div>
-
-  
-
-</div>
-
-</div>
+   
