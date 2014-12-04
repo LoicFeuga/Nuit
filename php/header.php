484705cad7c3dd8ce74103119d63dd35c79fbb4d
Loïc <loic.feuga@gmail.com>
2014-12-04 20:21:30 +0100

    Mise en place d'un header

---
+++ php/header.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+
+<html lang="fr">
+
+<head>
+    <meta http-equiv="Content-Type" content="charset=UTF8">
+    <title>Générateur HTML</title>
+
+    <!-- Inclusion des pages de styles -->
+    <link rel="stylesheet" type="text/css" href="css/index.css">
+    
+    <!-- Inclusion de BootStrap -->
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+</head>
+
+<body>
+
+   <header class="navbar navbar-static-top bs-docs-nav" id="top" role="banner">
+      <div class="container">
+        <div class="navbar-header">
+          <button class="navbar-toggle collapsed" type="button" data-toggle="collapse" data-target=".bs-navbar-collapse">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+        </button>
+        <a href="../" class="navbar-brand">Logo</a>
+    </div>
+   <nav class="collapse navbar-collapse bs-navbar-collapse" role="navigation">
+      <ul class="nav navbar-nav">
+        <li>
+          <a href="" class="center">Information Humanitaire</a>
+        </li>
+
+
+      </ul>
+      <ul class="nav navbar-nav navbar-right">
+        <li><a href="http://expo.getbootstrap.com" onclick="">Inscription</a></li>
+        <li><a href="http://blog.getbootstrap.com" onclick="">Connexion</a></li>
+      </ul>
+    </nav>
+</div>
+</header>
+
