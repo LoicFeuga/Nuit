730d8deb31a74237b098716cdfd98326ea6479f1
Loïc <loic.feuga@gmail.com>
2014-12-04 20:22:12 +0100

    mise en place d'un footer

---
+++ php/footer.php
@@ -0,0 +1,19 @@
+
+<!-- Inclusion de jquery -->
+<script src="https://code.jquery.com/jquery.min.js"></script>
+
+    <!-- Incusion de jquery en local 
+    <script type="text/javascript" src =""></script>-->
+
+    <!-- Inclusion des pages JavaScript -->
+    <script src="js/index.js"></script>
+
+    <!-- Inclusion des pages jQuery-->
+    <script src="js/jquery/index_jquery.js"></script>
+    
+    <!-- Inclusion de BootStrap js -->
+    <script src="js/bootstrap.min.js"></script>
+
+</body>
+
+</html>
