1e48ed41809485322fe7d919800f4d1436f01feb
leopaul29 <loic.feuga@gmail.com>
2014-12-04 20:49:35 +0100

    Mise en place des 1er header de Accueil, Famille,Camps

---
+++ lescamps.php
@@ -0,0 +1,22 @@
+
+<?php include "php/header.php"; ?>
+
+    <div class="row" id="header_camp" class=""> 
+      <div class="col-lg-12">
+        C'est les camps  
+      </div>
+    </div>
+
+
+
+
+
+  </div>
+
+  
+
+</div>
+
+</div>
+
+<?php include "php/footer.php"; ?>
