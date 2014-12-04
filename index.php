15ef7395e20ebbd2cad04c23c170c445b58560a5
Loïc <loic.feuga@gmail.com>
2014-12-04 20:01:33 +0100

    Archi

---
+++ index.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+
+<html lang="fr">
+
+<head>
+    <meta http-equiv="Content-Type" content="charset=UTF8">
+    <title>Générateur HTML</title>
+
+    <!-- Inclusion des pages de styles -->
+    <link rel="stylesheet" type="text/css" href="css/index.css">
+    
+    <!-- Inclusion de BootStrap -->
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+</head>
+
+<body>
+
+   <header class="navbar navbar-static-top bs-docs-nav" id="top" role="banner">
+      <div class="container">
+        <div class="navbar-header">
+          <button class="navbar-toggle collapsed" type="button" data-toggle="collapse" data-target=".bs-navbar-collapse">
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+        </button>
+        <a href="../" class="navbar-brand">Logo</a>
+    </div>
+    <nav class="collapse navbar-collapse bs-navbar-collapse" role="navigation">
+        Information Humanitaire
+        <ul class="nav navbar-nav navbar-right">
+            <li><a href="http://expo.getbootstrap.com" onclick="">Inscription</a></li>
+            <li><a href="http://blog.getbootstrap.com" onclick="">Connexion</a></li>
+        </ul>
+    </nav>
+</div>
+</header>
+
+
+
+
+
+
+<!-- Inclusion de jquery -->
+<script src="https://code.jquery.com/jquery.min.js"></script>
+
+    <!-- Incusion de jquery en local 
+    <script type="text/javascript" src =""></script>-->
+
+    <!-- Inclusion des pages JavaScript -->
+    <script src="js/index.js"></script>
+
+    <!-- Inclusion des pages jQuery-->
+    <script src="js/jquery/index_jquery.js"></script>
+    
+    <!-- Inclusion de BootStrap js -->
+    <script src="js/bootstrap.min.js"></script>
+
+</body>
+
+</html>
