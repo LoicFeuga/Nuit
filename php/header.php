205091b05738efb2c626508e33519286f1053aa8
Loïc <loic.feuga@gmail.com>
2014-12-04 20:34:59 +0100

    Correction du css + ajout des 2eme headers en none

--- php/header.php
+++ php/header.php
@@ -35,10 +35,70 @@
 
       </ul>
       <ul class="nav navbar-nav navbar-right">
-        <li><a href="http://expo.getbootstrap.com" onclick="">Inscription</a></li>
-        <li><a href="http://blog.getbootstrap.com" onclick="">Connexion</a></li>
+        <li><a href="" onclick="">Inscription</a></li>
+        <li><a href="" onclick="">Connexion</a></li>
       </ul>
     </nav>
 </div>
 </header>
 
+<div class="container">
+<div class="row">
+  <div class="col-lg-12">
+    <div class="row">
+      <div class="col-lg-12"> 
+        <div class="col-lg-2">Accueil</div>
+        <div class="col-lg-2">Ma famille</div>
+        <div class="col-lg-2">Les camps</div>
+        <div class="col-lg-2">Devenir bénévoles</div>
+        <div class="col-lg-2">Missions</div>
+        <div class="col-lg-2">Contact</div>
+      </div>
+    </div>
+
+    <div class="row" id="header_accueil" class=""> 
+      <div class="col-lg-12 header_multi">
+        C'est l'accueil  
+      </div>
+    </div>
+  
+    <div class="row" id="header_famille" class=""> 
+      <div class="col-lg-12 header_multi">
+        C'est ma famille  
+      </div>
+    </div>
+    <div class="row" id="header_camp" class=""> 
+      <div class="col-lg-12 header_multi">
+        C'est les camps  
+      </div>
+    </div>
+
+     <div class="row" id="header_benevole" class=""> 
+      <div class="col-lg-12 header_multi">
+        C'est pour devenir bénévoles
+      </div>
+    </div>
+
+     <div class="row" id="header_mission" class=""> 
+      <div class="col-lg-12 header_multi">
+        C'est pour les missions
+      </div>
+    </div>
+
+    
+  
+ <div class="row" id="header_contact" class=""> 
+      <div class="col-lg-12 header_multi">
+        C'est les contacts
+      </div>
+    </div>
+
+
+
+  </div>
+
+  
+
+</div>
+
+</div>
