42b671e5ef3e49c7fb1bf01cbbc34e27247a65bd
leopaul29 <leopaulmartin@hotmail.fr>
2014-12-05 02:42:50 +0100

    css form inscription a regler

--- inscription.php
+++ inscription.php
@@ -150,10 +150,23 @@ input[type=password]{
 
 .gender {
   margin-left: 30px;
-  margin-bottom: 30px;
+  margin-top: 20px;
 }
 
-.accounttype{
+.malade {
+  margin-left: 8px;
+  margin-top: 20px;
+}
+.malade label.radio:before {
+  background: #FF0000;
+}
+
+.vaccin {
+  margin-left: 8px;
+  margin-top: 20px;
+}
+
+.priorite {
   margin-left: 8px;
   margin-top: 20px;
 }
@@ -202,34 +215,51 @@ a.button:hover {
 <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,300italic,400italic,600' rel='stylesheet' type='text/css'>
 <link href="//netdna.bootstrapcdn.com/font-awesome/3.1.1/css/font-awesome.css" rel="stylesheet">
 
-<div class="testbox">
-  <h1>Registration</h1>
 
-  <form action="/">
-      <hr>
-    <div class="accounttype">
-      <input type="radio" value="None" id="radioOne" name="account" checked/>
-      <label for="radioOne" class="radio" chec>Intervenant</label>
-      <input type="radio" value="None" id="radioTwo" name="account" />
-      <label for="radioTwo" class="radio">Perdu</label>
-    </div>
-  <hr>
-  <label id="icon" for="name"><i class="icon-envelope "></i></label>
-  <input type="text" name="name" id="name" placeholder="Email" required/>
-  <label id="icon" for="name"><i class="icon-user"></i></label>
-  <input type="text" name="name" id="name" placeholder="Name" required/>
-  <label id="icon" for="name"><i class="icon-shield"></i></label>
-  <input type="password" name="name" id="name" placeholder="Password" required/>
-  <div class="gender">
-    <input type="radio" value="None" id="male" name="gender" checked/>
-    <label for="male" class="radio" chec>Male</label>
-    <input type="radio" value="None" id="female" name="gender" />
-    <label for="female" class="radio">Female</label>
-   </div> 
-   <p>By clicking Register, you agree on our <a href="#">terms and condition</a>.</p>
-   <a href="#" class="button">Register</a>
-  </form>
+<h1>Inscription</h1>
+
+<div align="center">
+	<form action="/">
+	<hr>
+	<label id="icon" for="name"><i class="icon-user "></i></label>
+	<input type="text" name="name" id="name" placeholder="First-name" required/></br>
+	<label id="icon" for="name"><i class="icon-user"></i></label>
+	<input type="text" name="name" id="name" placeholder="Last-name" required/>
+	</hr>
+	<hr>
+	<div class="gender">
+	<input type="radio" value="None" id="male" name="gender" checked/>
+	<label for="male" class="radio" chec>Male</label>
+	<input type="radio" value="None" id="female" name="gender" />
+	<label for="female" class="radio">Female</label>
+	</div>
+	</hr>
+	<hr>
+	<div class="malade">
+	  <input type="radio" value="None" id="malade_y" name="malade" checked/>
+	  <label for="malade_y" class="radio" chec>Malade</label>
+	  <input type="radio" value="None" id="malade_n" name="malade" />
+	  <label for="malade_n" class="radio" chec></label>
+	</div>
+	<div class="vaccin">
+	  <input type="radio" value="None" id="vaccin_y" name="vaccin" checked/>
+	  <label for="vaccin_y" class="radio" chec>Vacciné</label>
+	  <input type="radio" value="None" id="vaccin_n" name="vaccin" />
+	  <label for="vaccin_n" class="radio" chec></label>
+	</div>
+	<div class="priorite">
+	  <input type="radio" value="None" id="priorite_y" name="priorite" checked/>
+	  <label for="priorite_y" class="radio" chec>Prioritaire</label>
+	  <input type="radio" value="None" id="priorite_n" name="priorite" />
+	  <label for="priorite_n" class="radio" chec></label>
+	</div>
+	<hr>
+
+	<p>By clicking Register, you agree on our <a href="#">terms and condition</a>.</p>
+	<a href="#" class="button">Register</a>
+	</form>
 </div>
+
 
 
 <?php include "php/footer.php"; ?>
