f4007770db093c4e4ac50727ba52ebde3226da3c
Loïc <loic.feuga@gmail.com>
2014-12-05 02:56:13 +0100

    co

---
+++ connexion.php
@@ -0,0 +1,27 @@
+
+<?php include "php/header.php"; 
+
+
+include "php/connexion.php";
+
+
+?>
+
+
+
+
+<div id="tooplate_main">
+    
+    <div id="tooplate_content">
+
+		Id : <input type="text" id="connexion-id">
+
+		<button id="connexion-personne">Valider</button>
+            
+            
+    </div>
+    
+</div>
+
+
+<?php include "php/footer.php"; ?>
